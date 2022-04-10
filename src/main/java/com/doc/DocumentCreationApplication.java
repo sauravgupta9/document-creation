38c53e6e64aeb52d740a1f8f201d8c1583d87319
sauravgupta9 <88975445+sauravgupta9@users.noreply.github.com>
2022-04-10 15:10:05 +0530

    First commit

---
+++ src/main/java/com/doc/DocumentCreationApplication.java
@@ -0,0 +1,13 @@
+package com.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentCreationApplication.class, args);
+	}
+
+}
